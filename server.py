7e19d06965a94d99c1a930f05ed4804c32c0d160
Amos Tin <amostin@Amosde-MacBook-Air.local>
2020-10-02 17:38:45 +0800

    my web app files

---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect, request
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+	return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+	return render_template(page_name)
+
+
+def write_to_csv(data):
+	with open("database.csv", mode= 'a') as database:
+		column= ['email','subject','message']
+		email = data["email"]
+		subject = data['subject']
+		message = data['message']
+		csv_writer = csv.DictWriter(database,fieldnames= column, delimiter=',',quotechar='"',quoting =csv.QUOTE_MINIMAL )
+		
+		csv_writer.writerow({'email': email, 'subject':subject, 'message': message})
+
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	if request.method == 'POST':
+		data = request.form.to_dict()
+		write_to_csv(data)
+		return redirect('/thankyou.html')
+
+	else:
+		return 'sth is wrong'
+
